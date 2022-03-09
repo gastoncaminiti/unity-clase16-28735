db79c672c25f35277514771e4787d1500f03cda4
Gaston Caminiti <gastoncaminiti@gmail.com>
2022-03-09 10:44:07 -0300

    Proyecto Inicial
    Base Clase 16

---
+++ CLASE 16 - SCRIPT Y UI/Assets/UI/Scripts/HUDManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    [SerializeField] private Text textGemBlue;
+    [SerializeField] private Text textGemGreen;
+    [SerializeField] private Text textGemPink;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ACTUALIZAR LA INTERFAZ\
+        UpdateGemUI();
+    }
+
+    private void UpdateGemUI()
+    {
+       int[] GemCounts = GameManager.instance.gemQuantity;
+       textGemBlue.text = "x "+ GemCounts[0];
+       textGemGreen.text = "x "+ GemCounts[1];
+       textGemPink.text = "x "+ GemCounts[2];
+
+    }
+}
